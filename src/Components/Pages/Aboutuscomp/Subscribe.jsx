fd4095628c59fb79880ffd9cb9b16a0b22ef0aac
Permender Kumar <permender1998@gmail.com>
2022-03-13 23:28:29 +0530

    updated form working function

--- src/Components/Pages/Aboutuscomp/Subscribe.jsx
+++ src/Components/Pages/Aboutuscomp/Subscribe.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import $ from 'jquery';
 // import { MDBContainer, MDBInputGroup, MDBBtn, MDBInput } from "mdbreact";
 import { Button } from '@mui/material';
 // import { MDBContainer, MDBInputGroup, MDBDropdown, MDBDropdownToggle, MDBIcon, MDBDropdownMenu, MDBDropdownItem } from "mdbreact";
@@ -10,6 +11,48 @@ import { FaFacebookF, FaLinkedinIn } from 'react-icons/fa';
 
 
 function Subscribe() {
+    const PostToGoogle = async (e) => {
+        e.preventDefault();
+        // var field1 = $("#nameField").val();
+        var field2 = $("#emailField").val();
+        // var field3 = $("#mobField").val();
+        // var field4 = $("#cinema").val();
+
+        // if (field1 === "") {
+        //     alert('Please Fill Your Name');
+        //     document.getElementById("nameField").focus();
+        //     return false;
+        // }
+        if (field2 === "") {
+            alert('Please Fill Your Email');
+            document.getElementById("emailField").focus();
+            return false;
+        }
+        // if (field3 === ""
+        // ) {
+        //     alert('Please Fill Your Mobile Number');
+        //     document.getElementById("mobField").focus();
+        //     return false;
+        // }
+
+
+        $.ajax({
+            url: "https://docs.google.com/forms/u/5/d/e/1FAIpQLSe28W5GxgYb0s_jBIIxtMmLHBtnW4rK_R_LrIQPsshqHpvA4Q/formResponse?",
+            // url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSchGAXI0z8Qsw0na34X-zQwWRc5srn9cmFgkAClYVSrIdcDgg/formResponse?",
+            data: {  "entry.525768553": field2},
+            type: "POST",
+            dataType: "xml",
+            success: function (d) {
+            },
+            error: function (x, y, z) {
+
+                $('#success-msg1').show();
+                $('#form').hide();
+
+            }
+        });
+        return false;
+    };
     return (
         <div className='subscribe'>
             <div className="container">
@@ -32,10 +75,14 @@ function Subscribe() {
                                     }}
                                     noValidate
                                     autoComplete="off"
+                                    id="form" target="_self" onSubmit={PostToGoogle} action="" autocomplete="off" 
                                 >
-                                    <TextField label="Email Id" color="secondary" focused className='textfieldform' />
-                                    <Button type="submit" variant="contained">Subscribe</Button>
+                                    <TextField label="Email Id" color="secondary" focused className='textfieldform' id="emailField" name="entry.525768553" />
+                                    <Button type="submit" variant="contained" id="send" value="Submit" >Subscribe</Button>
                                 </Box>
+                                <h3 id="success-msg1">
+                                    You have successfully subscibed ARDOCK newsletter. 
+                                </h3>
                             </div>
                             <div className="formsocial"><div className="social">
                                 <a href="https://www.facebook.com/" target="_blank" >
--- src/Components/Pages/Contactus/Contactusform.jsx
+++ src/Components/Pages/Contactus/Contactusform.jsx
@@ -4,16 +4,60 @@ import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn, MDBCard, MDBCardBody, M
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 
-
+import $ from 'jquery';
 
 const FormPage = () => {
+    const PostToGoogle = async (e) => {
+        e.preventDefault();
+        var field1 = $("#nameField").val();
+        var field2 = $("#emailField").val();
+        var field3 = $("#mobField").val();
+        var field4 = $("#cinema").val();
+
+        if (field1 === "") {
+            alert('Please Fill Your Name');
+            document.getElementById("nameField").focus();
+            return false;
+        }
+        if (field2 === "") {
+            alert('Please Fill Your Email');
+            document.getElementById("emailField").focus();
+            return false;
+        }
+        if (field3 === "" 
+        // || field3.length > 10 || field3.length < 10
+        ) {
+            alert('Please Fill Your Mobile Number');
+            document.getElementById("mobField").focus();
+            return false;
+        }
+
+
+        $.ajax({
+            url: "https://docs.google.com/forms/u/5/d/e/1FAIpQLSeytHwjpqu2T_kwvLYbkEU4MH7erHEPypW4omWpjT2Be6EYwA/formResponse?",
+            // url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSchGAXI0z8Qsw0na34X-zQwWRc5srn9cmFgkAClYVSrIdcDgg/formResponse?",
+            data: { "entry.1481576165": field1, "entry.132917256": field2, "entry.1116563792": field3, "entry.1439449117": field4 },
+            type: "POST",
+            dataType: "xml",
+            success: function (d) {
+            },
+            error: function (x, y, z) {
+
+                $('#success-msg').show();
+                $('#form').hide();
+
+            }
+        });
+        return false;
+    };
+
     return (
         <MDBContainer className="upr"       >
             <MDBRow    className="upr3" >
                 <MDBCol md="12">
                     <MDBCard className="upr2">
                         <MDBCardBody className="upr1" >
-                            <form>
+                            <form id="form" target="_self" onSubmit={PostToGoogle} action="" autocomplete="off" >
                                 {/* <h1 className="text-center py-4">Sign up</h1> */}
                                 <div className="grey-text">
                                     <MDBInput
@@ -24,6 +68,8 @@ const FormPage = () => {
                                         validate
                                         error="wrong"
                                         success="right"
+                                        id="nameField" 
+                                        name="entry.1958936365"
                                     />
                                     <MDBInput
                                         label="Your email"
@@ -33,14 +79,16 @@ const FormPage = () => {
                                         validate
                                         error="wrong"
                                         success="right"
+                                        id="emailField" 
+                                        name="entry.732086521" 
                                     />
-                                    <MDBInput label="Subject" icon="tag" group type="text" validate error="wrong" success="right" />
-                                    <MDBInput type="textarea" rows="5" label="Your message" icon="pencil-alt" group  validate error="wrong" success="right"/>
+                                    <MDBInput id="mobField" name="entry.556343013"  label="Subject" icon="tag" group type="text" validate error="wrong" success="right" />
+                                    <MDBInput id="cinema" name="entry.555471935" type="textarea" rows="5" label="Your message" icon="pencil-alt" group  validate error="wrong" success="right"/>
                                 </div>
                                 <div className="text-center py-4 mt-3">
                                     <div className="firstbut">
                                         <Stack direction="row" spacing={2}>
-                                            <Button color="secondary">
+                                            <Button color="secondary" id="send" type="submit" value="Submit" >
                                                 Submit
                                             </Button>
                                         </Stack>
@@ -54,6 +102,7 @@ const FormPage = () => {
                                     </MDBBtn> */}
                                 </div>
                             </form>
+                            <h3 id="success-msg">Your Request has been posted to Google Spreadsheet</h3>
                         </MDBCardBody>
                     </MDBCard>
                 </MDBCol>
